38eff0cfc2f0338f86f7fc6c0dae101b9b49a468
mc12s <mc12s@my.fsu.edu>
2017-10-26 13:57:55 -0400

    initial

---
+++ Assets/scripts/LevelSetup.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSetup : MonoBehaviour {
+
+	public GameObject planet;
+	public GameObject player;
+	public GameObject planet_text;
+
+	public int planet_count=8;
+	public int planet_size_max=10;
+	public int planet_size_min=2;
+
+
+	// Use this for initialization
+	void Awake () {
+		Canvas myCanvas = FindObjectOfType<Canvas>();
+		for(int i=0; i<planet_count; i++){
+			float planet_size = Random.Range(planet_size_min, planet_size_max);
+
+			Vector2 planet_loc = planetPositioner(planet_size/2); //recursive function for positioning nonoverlapping planets
+
+			GameObject newPlanet = Instantiate(planet, planet_loc, Quaternion.identity);	//random position
+			newPlanet.transform.localScale = new Vector3(planet_size, planet_size, 0f);		//random size
+			newPlanet.GetComponent<SpriteRenderer>().color = new Color(Random.value,Random.value,Random.value); //random color
+
+			//attach text to each planet
+			GameObject newPlanetText = Instantiate(planet_text, planet_loc, Quaternion.identity);
+			newPlanetText.transform.SetParent(myCanvas.transform);
+			newPlanetText.GetComponent<Text>().text = planet_size.ToString();
+
+
+			if(planet_size>=9){
+				//for(int j=0; j<Random.Range(1,3);j++){	//set for random number of moons
+					Vector2 moonOffset = Random.insideUnitCircle;
+					Vector2 moonOrbit = moonOffset + (moonOffset.normalized * planet_size/2);
+					GameObject newMoon = Instantiate(planet, planet_loc+moonOrbit, Quaternion.identity);
+					newMoon.transform.localScale = new Vector3(1.4f, 1.4f, 0f);
+					newMoon.GetComponent<SpriteRenderer>().color = new Color(Random.value,Random.value,Random.value);
+					newMoon.GetComponent<SpriteRenderer>().sortingOrder = 2;
+					newMoon.layer = 18;
+					newMoon.tag = "moon";
+				//}
+			}
+		}
+	}
+
+	void Start(){	//Spawn in players and set their colors
+		Vector2 spawn = new Vector2(-20f,-10f); //init position offset
+
+		//P1, instantiate, color, and rename
+		GameObject p1 = Instantiate(player, Vector2.Scale(spawn,new Vector2(1,1)), Quaternion.identity); 
+		GameObject p1model = p1.transform.Find("player").gameObject;
+		p1model.GetComponent<SpriteRenderer>().color = new Color(0,1,1,1);
+		p1model.layer = 8;	//taken from layer setup
+		p1model.name = "p1";
+
+		//P2, instantiate, color, and rename
+		GameObject p2 = Instantiate(player, Vector2.Scale(spawn,new Vector2(-1,1)), Quaternion.identity); 
+		GameObject p2model = p2.transform.Find("player").gameObject;
+		p2model.GetComponent<SpriteRenderer>().color = new Color(0,1,0,1);
+		p2model.layer = 10;
+		p2model.name = "p2";
+
+		//P3, instantiate, color, and rename
+		GameObject p3 = Instantiate(player, Vector2.Scale(spawn,new Vector2(1,-1)), Quaternion.identity); 
+		GameObject p3model = p3.transform.Find("player").gameObject;
+		p3model.GetComponent<SpriteRenderer>().color = new Color(1,0,1,1);
+		p3model.layer = 12;
+		p3model.name = "p3";
+
+		//P4, instantiate, color, and rename
+		GameObject p4 = Instantiate(player, Vector2.Scale(spawn,new Vector2(-1,-1)), Quaternion.identity); 
+		GameObject p4model = p4.transform.Find("player").gameObject;
+		p4model.GetComponent<SpriteRenderer>().color = new Color(1,0,0,1);
+		p4model.layer = 14;
+		p4model.name = "p4";
+
+	}
+
+	Vector3 planetPositioner(float radius){	//recursive function, what happens if planets randomly spawn ontop of eachother?
+		Vector2 planet_loc = new Vector2(Random.Range(-35,35),Random.Range(-20,20));
+		//if planet loc+scale overlaps with any other planet recurse
+		if(Physics2D.OverlapCircle(planet_loc, radius+1)){
+			return planetPositioner(radius);
+		}
+		return planet_loc;
+	}
+
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject player_aim;
+	public GameObject thruster_obj;
+	public GameObject bullet;
+
+	public float shotspeed = 40f;
+	public float boost = 500f;
+	public int health = 100;
+
+	public AudioClip fire_sound;
+	public AudioClip die_sound;
+	public GameObject explosion;
+
+	private GameObject[] objs;
+	private Rigidbody2D rb;
+	private ParticleSystem thruster;
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		objs = GameObject.FindGameObjectsWithTag("planet");
+		rb = GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>();
+		thruster = thruster_obj.GetComponent<ParticleSystem>();
+		thruster.Stop();	//stop particle system
+	}
+
+	void Update(){
+		if(Input.GetButtonUp(gameObject.name +"Jump")){
+			thruster.Stop();	//stop the particle system when you let go of boost
+		}
+		player_aim.transform.position = transform.position;
+	}
+
+	void FixedUpdate () {
+		//Gravity Calculations
+		foreach(GameObject planet in objs){	//for every planet
+			float planet_mass=Mathf.Pow((planet.transform.localScale.x*Mathf.PI),2f)*50f; //increased mass to counter closeness of planets
+			Vector3 heading = planet.transform.position - transform.position; //vector from planet to player
+			float distance = heading.magnitude; //distance from player to planet
+			float planet_gravity = planet_mass/(Mathf.Pow(distance*5f, 2f));    // GMm/d^2, artificially increased distance
+			Vector3 direction = heading/distance;		//a vector divided by its magnitude is a unit vector
+			rb.AddForce(direction * planet_gravity);	//pull player towards planet with calculated gravity
+
+		} //end of gravity loop
+
+
+
+
+		//Get Inputs
+		float moveH = Input.GetAxis(gameObject.name + "Horizontal");
+		float moveV = Input.GetAxis(gameObject.name + "Vertical");
+
+		Vector2 movement = new Vector2(moveH,moveV);
+		rb.AddForce(movement * 50f * Time.fixedDeltaTime); //move player without boost
+
+		if(Input.GetButton(gameObject.name + "Jump")){ //Move with boost
+			float angle = Mathf.Atan2(moveV,moveH) * (180f/Mathf.PI);	//convert input axes into degrees
+			player_aim.transform.eulerAngles = new Vector3(0f,0f,angle-180f);
+			thruster.Play(); //starts the particle effect
+			rb.AddForce(movement * boost * Time.fixedDeltaTime);
+		}
+
+
+
+
+		if(Input.GetButtonDown(gameObject.name + "Fire1")){
+			if(movement.magnitude != 0){								//if we're holding a direction and pressing fire
+				GameObject laser = Instantiate(bullet,transform.position,Quaternion.identity); 	//create a bullet
+				laser.layer = gameObject.layer + 1;												//put it on the correct layer
+				laser.GetComponent<Rigidbody2D>().velocity = movement * shotspeed;				//fire it off in the right direction
+				AudioSource.PlayClipAtPoint(fire_sound, transform.position);					//play a sound
+
+			}
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.CompareTag("bullet")){			//if you get shot
+			print("hit");
+			health-=40;
+			Destroy(other.gameObject);
+		}
+		else if(other.CompareTag("shredder")){	//if you stray too far from the playing field
+			health-=100;
+		}
+		if(health<=0){
+			Die();
+		}
+	}
+
+	void Die(){
+		AudioSource.PlayClipAtPoint(die_sound,transform.position);	//play explosion sound
+		GameObject explode = Instantiate(explosion, transform.position, Quaternion.identity);	//load explosion
+		ParticleSystem.MainModule p_main= explode.GetComponent<ParticleSystem>().main; 
+		p_main.startColor = sr.color;	//color the explosion the same as the player
+		Destroy(transform.parent.gameObject);	//destroy the player
+	}
+}
